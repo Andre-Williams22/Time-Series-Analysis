ff7b2bf41663102ae8583fe99906bd9c6bfde96a
andre <awjrs22@gmail.com>
2020-08-09 18:33:44 -0400

    adds streamlit requirements

--- webapp.py
+++ webapp.py
@@ -9,7 +9,6 @@ st.write("""
 Shown are the stock **closing price** and **volume** of Google!
 """)
 
-# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
 #define the ticker symbol
 tickerSymbol = 'AAPL'
 #get data on this ticker
